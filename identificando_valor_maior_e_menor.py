4329ac0b410786c6f3bcf50faacd18502647969f
blancodudaa <dev.dudablanco@outlook.com>
2023-01-22 15:48:12 -0300

    subindo identificador de valores (maior e menor)

---
+++ identificando_valor_maior_e_menor.py
@@ -0,0 +1,11 @@
+valor1 = int(input())
+
+valor2 = int(input())
+
+if valor1 > valor2:
+  print("maior:", valor1)
+  print("menor:", valor2)
+
+else:
+  print("maior:", valor2)
+  print("menor:", valor1)
