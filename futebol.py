745ef570c75bf2629f92f870322dd938d93e7064
blancodudaa <dev.dudablanco@outlook.com>
2023-01-24 20:25:18 -0300

    alterações de código

---
+++ futebol.py
@@ -0,0 +1,24 @@
+time1= input()
+time2 = input()
+
+golt1=int(input())
+golt2=int(input())
+
+if golt1==0 and golt2==0:
+  print("prorrogação!")
+  pro1=int(input())
+  pro2=int(input())
+if pro1>pro2:
+  print("vitoria:", time1)
+  p1<p2 
+  print("vitoria:", time2 )
+
+elif p1==0 and p2==0:
+  print("disputa de penaltis!")
+
+
+if golt1>golt2:
+     print("vitoria:", time1)
+     
+else:
+    print("vitoria:", time2 )
